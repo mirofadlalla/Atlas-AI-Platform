200abc1deb90d45f392aa9c25e13b6b07159e27b
Omar Yaser <omaryaser311@gmail.com>
2026-08-08 23:33:21 +0300

    ci: remove security scan job from workflow

--- app/rag/retrivel_data_pipline.py
+++ app/rag/retrivel_data_pipline.py
@@ -3,8 +3,16 @@ import logging
 import hashlib
 import time
 from cachetools import TTLCache
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+try:
+    from langchain.chains import create_retrieval_chain
+    from langchain.chains.combine_documents import create_stuff_documents_chain
+except (ImportError, ModuleNotFoundError):
+    try:
+        from langchain.chains.retrieval import create_retrieval_chain
+        from langchain.chains.combine_documents import create_stuff_documents_chain
+    except (ImportError, ModuleNotFoundError):
+        create_retrieval_chain = None
+        create_stuff_documents_chain = None
 from langchain.prompts import ChatPromptTemplate
 from sqlalchemy.orm import Session
 
